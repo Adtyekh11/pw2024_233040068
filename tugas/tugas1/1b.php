f03509fcf3347528b09d012ae8d7249fb76e0c79
Adtyekh11 <aditya.233040068@mail.unpas.ac.id>
2024-02-19 21:31:02 +0700

    first commit

---
+++ tugas/tugas1/1b.php
@@ -0,0 +1,10 @@
+<?php
+$NPM = "68";
+
+echo "Aku adalah angka" . " " . "<strong>" . $NPM . "</strong>" . "<br>";
+echo "Jika aku dikali 5, maka aku sekarang menjadi " . "<strong>" . $NPM * 5 . "</strong>" . "<br>";
+echo "Jika aku dibagi 2, maka aku sekarang menjadi " .  "<strong>" . $NPM / 2 . "</strong>" . "<br>";
+echo "Jika aku ditambah 75, maka aku sekarang menjadi " . "<strong>" . $NPM + 75 . "</strong>" . "<br>";
+echo "Jika aku dikurang 20, maka aku sekarang menjadi " . "<strong>" . $NPM - 20 . "</strong>" . "<br>";
+
+?>
---
+++ tugas/tugas1/1c.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<head>
+    <title>Tugas 1</title>
+    <style>
+        * {
+            font-family: arial;
+            font-size: 20px
+        }
+
+        .tangga {
+            display:flex;
+            flex-direction:column;
+        }
+
+        .baris {
+            display:flex;
+            justify-content: flex-start;
+        }
+
+        .kotak {
+            display:flex;
+            justify-content: center;
+            align-items:center;
+            width:50px;
+            height:50px;
+            background-color:red;
+            border: 1.5px solid black;
+            margin: 0;
+        }
+    </style>
+
+    <head>
+        <div class="tangga">
+            <div class="baris">
+                <div class="kotak">1</div>
+            </div>
+            <div class="baris">
+                <div class="kotak">2</div>
+                <div class="kotak">2</div>
+            </div>
+            <div class="baris">
+                <div class="kotak">3</div>
+                <div class="kotak">3</div>
+                <div class="kotak">3</div>
+            </div>
+        </div>
+
+    </head>
+</head>
